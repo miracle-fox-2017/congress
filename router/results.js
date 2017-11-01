3acc5af7acd9d899325d27c6e002d8e227f5366a
yulianhp <kono.utsukushi24@gmail.com>
2017-11-01 23:30:33 +0700

    release 3 done

--- router/results.js
+++ router/results.js
@@ -2,17 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 const topFive = require('../models/topfive')
+const analyzed = require('../models/analyzed')
 
 router.get('/results/top5', function(req, res){
   topFive.calculate(function(hasil){
-    console.log(hasil);
-    
+    // console.log(hasil);
+    // let candidate = []
+    // for(let i = 0; i < hasil.length; i++){
+    //   if(candidate.indexOf(hasil[i].name) != -1){
+    //     candidate.push(hasil[i].name)
+    //   }
+    // }
+    // console.log(candidate);
     res.render('topfive', {hasil})
   })
 })
 
 router.get('/results/analyzed', function(req, res){
-    res.render('analyzed')
+  analyzed.calculate(function(hasil){
+    res.render('analyzed', {hasil})
+  })
 })
 
 module.exports = router
