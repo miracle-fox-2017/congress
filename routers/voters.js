95aea0f36303135cb7efbc179cdc8c6431fd3e28
trijadoel <triamri@gmail.com>
2017-11-01 22:33:24 +0700

    release 2 blm selesai

--- routers/voters.js
+++ routers/voters.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const Vote = require('../models/voters');
 
 router.get('/voters', function (req, res) {
-    res.render('voters');
+    let isEdit = { isEdit : false };
+    res.render('voters', { isEdit });
+});
+
+router.post('/voters', function (req, res) {
+    let set = {
+        name: req.body.name
+    }
+    if (req.body.get == 1){
+        Vote.findName(set).then((dataName) => {
+            // let set = { name : "name" };
+            let isEdit = { isEdit: true };
+            res.render('voters', { dataName, isEdit });
+        }).catch((err) => {
+
+        })
+    } else if (req.body.get == 2){
+        Vote.findGender(set).then((dataName) => {
+            // let set = { name : "name" };
+            let isEdit = { isEdit: true };
+            res.render('voters', { dataName, isEdit });
+        }).catch((err) => {
+
+        })
+    }
+    
 });
 
 module.exports = router;
