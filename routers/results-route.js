c4cbd9f28a7b46849967b923eda3c85dde5bbb4b
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-01 18:42:45 +0700

    Minor fix

--- routers/results-route.js
+++ routers/results-route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const VoteModel = require('../models/vote-model');
 const VoterModel = require('../models/voter-model');
+const congressModel = require('../models/congress-member-model');
 
 router.get('/', (req, res) => {
 	res.send('Results');
@@ -13,33 +14,7 @@ router.get('/top5', (req, res) => {
 			
 			VoteModel.findTop5()
 				.then((allTop5) => {
-					let politicianId = [];
-					allTop5.forEach( function(politician, index) {
-						politicianId.push(politician.id);
-					});
-
-					let uniquePoliticianId = Array.from(new Set(politicianId));
-					let arrWinner = [];
-
-					for (var i = 0; i < uniquePoliticianId.length; i++) {
-						let obj = {
-							id: uniquePoliticianId[i],
-							name: '',
-							voters: []
-						};
-
-						for (var j = 0; j < allTop5.length; j++) {
-							if (uniquePoliticianId[i] == allTop5[j].id) {
-								obj.name = allTop5[j].name;
-								obj.jumlah_vote_individu = allTop5[j].jumlah_vote_individu;
-								obj.voters.push(allTop5[j].first_name);
-							}
-						}
-
-						arrWinner.push(obj);
-					}
-
-					res.render('top5', {top5: arrWinner});
+					res.render('top5', {top5: congressModel.splitWinnerVoter(allTop5)});
 				})
 				.catch(err => res.send(err));
 
