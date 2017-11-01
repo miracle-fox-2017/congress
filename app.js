db66584c2e7ee54ae65e388a6c11d26c437b1af5
Faris Widyantho <radenfaris@gmail.com>
2017-11-01 17:46:59 +0700

    release 2

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app     = express()
+const bodyParser = require('body-parser');
 
 app.set('views', './views') // specify the views directory
 app.set('view engine', 'ejs') // register the template engine
+
+app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()) // parse application/json
 
 //route index
 var index = require ('./routers/index');
@@ -11,6 +15,11 @@ app.use('/', index);
 //route results
 var results = require ('./routers/results');
 app.use('/results', results);
+
+//voters
+var voters = require ('./routers/voters');
+app.use('/voters', voters);
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
