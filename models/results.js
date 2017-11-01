74398ddffb71b120e553e2496e62d06234058811
Ayunda <ayundahamzah@gmail.com>
2017-11-02 00:18:58 +0700

    revisi release 2 di top5 ejs

--- models/results.js
+++ models/results.js
@@ -4,26 +4,19 @@ const db = new sqlite3.Database('./db/congress_poll_results.db')
 class Result{
 
   static findTop5(callback){
-    db.all(`SELECT subquery.C, subquery.name, voters.first_name
-      FROM ( SELECT votes.politician_id, congress_members.name, COUNT ( votes.politician_id) C
-      FROM votes
-      JOIN voters ON votes.voter_id = voters.id
-      JOIN congress_members ON congress_members.id = votes.politician_id
-      GROUP BY congress_members.name
-      ORDER BY COUNT ( votes.politician_id)
-      DESC
-      LIMIT 5
-      ) subquery
-      JOIN votes ON votes.politician_id = subquery.politician_id
-      JOIN voters ON voters.id = votes.voter_id
-      ORDER BY subquery.C DESC, subquery.name ;`,function(err,rows){
-        // console.log(rows);
-        if(err){
-          callback(err,null)
-        }else{
-          callback(null,rows)
-        }
-      })
+    return new Promise ((resolve,reject)=>{
+      db.all(`SELECT getTotal.Total, getTotal.name, voters.first_name FROM (SELECT COUNT(votes.politician_id) AS Total, congress_members.name,congress_members.id FROM votes
+              INNER JOIN congress_members ON congress_members.id = votes.politician_id
+              GROUP BY votes.politician_id
+              ORDER BY Total DESC
+              LIMIT 5)  AS getTotal
+              INNER JOIN votes ON votes.politician_id = getTotal.id
+              INNER JOIN voters ON voters.id = votes.voter_id
+              ORDER BY Total DESC,name ASC;`,(err,rows)=>{
+                if (err){ reject(err) }
+                else { resolve(rows) }
+              })
+    })
   }
  static getAnalyzed(){
   return new Promise((resolve, reject) => {
--- routers/results.js
+++ routers/results.js
@@ -4,14 +4,38 @@ const router = express.Router();
 const Result = require('../models/results');
 
 
-router.get('/top5',function(req,res){
-  Result.findTop5(function(err,rows){
-    // console.log(rows);
-    res.render('top5',{rows})
+router.get('/top5',(req,res)=>{
+  Result.findTop5().then(data =>{
+
+// res.send(data)
+    var array =[];
+    let arr = [];
+    data.push('end')
+    for (var i = 0; i < data.length-1; i++) {
+
+      if(data[i].name != data[i+1].name){
+        let obj ={};
+        obj['Total'] = data[i].Total;
+        obj['Name'] = data[i].name;
+        obj['first_name'] = []
+        array.push(obj)
+      }
+    }
+    let counter = 0
+    for (var i = 0; i < data.length-1; i++) {
+      if(data[i].name == data[i+1].name){
+        array[counter].first_name.push(data[i].first_name)
+      }
+      else {
+        counter++
+      }
+    }
+res.render('top5',{top5 : array})
+  //
   })
+
+  // res.render('top5')
 })
-
-
 
 router.get('/analyzed', (req, res) => {
   Result.getAnalyzed()
