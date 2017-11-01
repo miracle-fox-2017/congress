11c7ac54edd212d9bd541df87b73944cc5b589c9
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-01 18:18:26 +0700

    Finish release 1

--- models/vote-model.js
+++ models/vote-model.js
@@ -19,11 +19,38 @@ class VoteModel {
 		});
 	}
 
+	static createTop5View() {
+		return new Promise((resolve, reject) => {
+			let db = new sqlite3.Database(dbName);
+			let sql = `CREATE VIEW IF NOT EXISTS election_winner AS
+						select congress_members.id, congress_members.name, 
+						(
+							select count(politician_id) AS totalVote
+							from votes where politician_id = congress_members.id
+						) as jumlah_vote_individu
+					from congress_members order by jumlah_vote_individu DESC LIMIT 5;`;
+
+			db.all(sql, (err, rows) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(rows);
+				}
+			})
+		});
+	}
+
 	static findTop5() {
 		return new Promise((resolve, reject) => {
 			let db = new sqlite3.Database(dbName);
-			let sql = `SELECT * FROM ${tableName} WHERE id = 2`;
-			db.get(sql, (err, rows) => {
+			let sqlWinner = `Select election_winner.id, election_winner.name, 
+								election_winner.jumlah_vote_individu, votes.voter_id, voters.first_name
+								From election_winner
+								INNER JOIN votes ON election_winner.id = votes.politician_id
+								INNER JOIN voters ON votes.voter_id = voters.id 
+							ORDER BY election_winner.jumlah_vote_individu DESC, election_winner.name ASC; `;
+
+			db.all(sqlWinner, (err, rows) => {
 				if (err) {
 					reject(err);
 				} else {
--- routers/results-route.js
+++ routers/results-route.js
@@ -8,10 +8,41 @@ router.get('/', (req, res) => {
 })
 
 router.get('/top5', (req, res) => {
-	VoteModel.findTop5()
-		.then((allTop5) => {
-			console.log(allTop5)
-			res.render('top5', {top5: allTop5});
+	VoteModel.createTop5View()
+		.then((success) =>{
+			
+			VoteModel.findTop5()
+				.then((allTop5) => {
+					let politicianId = [];
+					allTop5.forEach( function(politician, index) {
+						politicianId.push(politician.id);
+					});
+
+					let uniquePoliticianId = Array.from(new Set(politicianId));
+					let arrWinner = [];
+
+					for (var i = 0; i < uniquePoliticianId.length; i++) {
+						let obj = {
+							id: uniquePoliticianId[i],
+							name: '',
+							voters: []
+						};
+
+						for (var j = 0; j < allTop5.length; j++) {
+							if (uniquePoliticianId[i] == allTop5[j].id) {
+								obj.name = allTop5[j].name;
+								obj.jumlah_vote_individu = allTop5[j].jumlah_vote_individu;
+								obj.voters.push(allTop5[j].first_name);
+							}
+						}
+
+						arrWinner.push(obj);
+					}
+
+					res.render('top5', {top5: arrWinner});
+				})
+				.catch(err => res.send(err));
+
 		})
 		.catch(err => res.send(err));
 	
