4f77e173c5c3ae64f4f963391579925eddfc05e8
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-01 15:32:37 +0700

    Rilis 2 dulu done

--- models/voter-model.js
+++ models/voter-model.js
@@ -19,21 +19,68 @@ class VoterModel {
 					resolve(rows);
 				}
 			})	
+
+			db.close();
 		});
 	}
 
 	static findByName(name) {
 		return new Promise((resolve, reject) => {
 			let db = new sqlite3.Database(dbName);
-			let sql = `SELECT * FROM voters WHERE first_name LIKE '%${name}%';`;
+			let sql = `SELECT * FROM ${tableName} WHERE first_name LIKE '%${name}%' ORDER BY first_name ASC;`;
 
-			db.run(sql, (err, rows) => {
+			db.all(sql, (err, rows) => {
 				if (err) {
 					reject(err);
 				} else {
 					resolve(rows);
 				}
 			})
+
+			db.close();
+		});
+	}
+
+	
+	static findByGender(gender) {
+		return new Promise((resolve, reject) => {
+			let db = new sqlite3.Database(dbName);
+			let sql = `SELECT * FROM ${tableName} WHERE gender = '${gender}' ORDER BY first_name ASC`;
+			console.log(sql);
+			db.all(sql, (err, rows) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(rows);
+				}
+			})
+
+			db.close();
+		});
+	}
+
+	static findByAgeRange(minAge, maxAge) {
+		return new Promise((resolve, reject) => {
+			let db = new sqlite3.Database(dbName)
+			let sql = `SELECT * FROM ${tableName} WHERE age >= ${minAge}  AND age <= ${maxAge} ORDER BY age ASC;`;
+
+			if (maxAge == '') {
+				sql = `SELECT * FROM ${tableName} WHERE age >= ${minAge}  AND age <= ${minAge} ORDER BY age ASC;`;
+			}
+			
+			if (minAge == '') {
+				reject('Min age harus diisi!');
+			}
+
+			db.all(sql, (err, rows) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(rows);
+				}
+			})
+
+			db.close();
 		});
 	}
 }
