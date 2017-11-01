ef279e56c41a0d403808170bf6cf30ba7e7ce34c
yofri <yofriadiyahya@gmail.com>
2017-11-01 18:44:37 +0700

    Release 1 partial done

--- routes/top5.js
+++ routes/top5.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
+const Top5 = require('../models/top5')
 
 router.get('/', (req, res) => {
-  res.render('top5');
+  Top5.findTop5()
+    .then(rows => {
+      // res.send(rows);
+      res.render('top5', {top5: rows});
+    }).catch();
 });
 
 module.exports = router;
