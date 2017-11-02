b55bede1262ecc59d3e03f1f81a9652dd62c5800
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-11-02 07:27:46 +0700

    merapikan models voter

--- models/voter.js
+++ models/voter.js
@@ -2,9 +2,38 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/congress_poll_results.db');
 
 class Voter {
+  static search(data) {
+    return new Promise((resolve, reject) => {
+      if(data.searchName) {
+        this.searchName(data.searchName).then(dataFilter => {
+          resolve(dataFilter)
+        }).catch(error => {
+          reject(error)
+        })
+      } else if(data.searchGender) {
+        this.searchGender(data.searchGender).then(dataFilter => {
+          resolve(dataFilter)
+        }).catch(error => {
+          reject(error)
+        })
+      } else if(data.minAge || data.maxAge) {
+        this.searchGender(data).then(dataFilter => {
+          resolve(dataFilter)
+        }).catch(error => {
+          reject(error)
+        })
+      }
+    })
+  }
+
   static searchName(data) {
     return new Promise((resolve, reject) => {
-      db.all(`SELECT * FROM voters WHERE first_name LIKE '%${data}%' OR last_name LIKE '%${data}%'`, (err, rows) => {
+      db.all(`SELECT voters.*,
+      CASE
+          WHEN voters.married = 0 THEN 'single/widow'
+          WHEN voters.married = 1 THEN 'married'
+      END AS married_stats
+      FROM voters WHERE first_name LIKE '%${data}%' OR last_name LIKE '%${data}%'`, (err, rows) => {
         if(err) {
           reject(err)
         } else {
@@ -16,7 +45,12 @@ class Voter {
 
   static searchGender(data) {
     return new Promise((resolve, reject) => {
-      db.all(`SELECT * FROM voters WHERE gender = '${data}'`, (err, rows) => {
+      db.all(`SELECT voters.*,
+      CASE
+          WHEN voters.married = 0 THEN 'single/widow'
+          WHEN voters.married = 1 THEN 'married'
+      END AS married_stats
+      FROM voters WHERE gender = '${data}'`, (err, rows) => {
         if(err) {
           reject(err)
         } else {
@@ -27,10 +61,15 @@ class Voter {
   }
 
   static searchAge(data) {
-    console.log(data);
+    // console.log(data);
     return new Promise((resolve, reject) => {
       if(data.maxAge) {
-        db.all(`SELECT * FROM voters WHERE age BETWEEN ${data.minAge} AND ${data.maxAge} ORDER BY age`, (err, rows) => {
+        db.all(`SELECT voters.*,
+        CASE
+            WHEN voters.married = 0 THEN 'single/widow'
+            WHEN voters.married = 1 THEN 'married'
+        END AS married_stats
+        FROM voters WHERE age BETWEEN ${data.minAge} AND ${data.maxAge} ORDER BY age`, (err, rows) => {
           if(err) {
             reject(err)
           } else {
@@ -38,7 +77,12 @@ class Voter {
           }
         })
       } else if(data.minAge){
-        db.all(`SELECT * FROM voters WHERE age = ${data.minAge} ORDER BY age;`, (err, rows) => {
+        db.all(`SELECT voters.*,
+        CASE
+            WHEN voters.married = 0 THEN 'single/widow'
+            WHEN voters.married = 1 THEN 'married'
+        END AS married_stats
+        FROM voters WHERE age = ${data.minAge} ORDER BY age;`, (err, rows) => {
           if(err) {
             reject(err)
           } else {
--- routers/voter.js
+++ routers/voter.js
@@ -5,33 +5,54 @@ const router = express.Router()
 
 // define the voter page route
 router.get('/', function (req, res) {
-  res.render('voter', {error: false, dataVoter: false})
+  res.render('voter', {error: '', dataVoter: ''})
 })
 
 router.post('/', function (req, res) {
-  if(!req.body.searchGender && !req.body.searchName && !req.body.minAge && !req.body.maxAge) {
-    res.render('voter', {error: true, dataVoter: false})
-  }
-
-  if(req.body.searchName) {
-    Voter.searchName(req.body.searchName).then(dataVoter => {
-      res.render('voter', {error: false, dataVoter: dataVoter})
-    })
-  }
-
-  if(req.body.searchGender) {
-    Voter.searchGender(req.body.searchGender).then(dataVoter => {
-      res.render('voter', {error: false, dataVoter: dataVoter})
-    })
-  }
-
-  if(req.body.maxAge || req.body.minAge) {
-    Voter.searchAge(req.body).then(dataVoter => {
-      res.render('voter', {error: false, dataVoter: dataVoter})
+  console.log(req.body);
+  if(!req.body.searchBy && !req.body.searchName && !req.body.searchGender && !req.body.minAge && !req.body.maxAge) {
+    // handle error jika field select kosong
+    res.render('voter', {error: 'Please fill the blank', dataVoter: false})
+  } else if(req.body.searchBy && !req.body.searchName && !req.body.searchGender && !req.body.minAge && !req.body.maxAge) {
+    // handle error jika field gender kosong
+    res.render('voter', {error: 'Please fill the blank', dataVoter: false})
+  } else if(req.body.searchBy && !req.body.searchName && !req.body.searchGender && !req.body.minAge && req.body.maxAge) {
+    // handle error jika field minAge kosong
+    res.render('voter', {error: 'Range Start Age must be filled', dataVoter: false})
+  } else {
+    Voter.search(req.body).then(dataFilter => {
+      res.render('voter', {error: '', dataVoter: dataFilter})
     }).catch(error => {
-      res.render('voter', {error: true, dataVoter: false})
+      res.render('voter', {error: 'Range Start Age must be filled', dataVoter: ''})
     })
   }
 })
 
 module.exports = router
+
+/*
+#Versi amburadul
+if(!req.body.searchGender && !req.body.searchName && !req.body.minAge && !req.body.maxAge) {
+  res.render('voter', {error: true, dataVoter: false})
+}
+
+if(req.body.searchName) {
+  Voter.searchName(req.body.searchName).then(dataVoter => {
+    res.render('voter', {error: false, dataVoter: dataVoter})
+  })
+}
+
+if(req.body.searchGender) {
+  Voter.searchGender(req.body.searchGender).then(dataVoter => {
+    res.render('voter', {error: false, dataVoter: dataVoter})
+  })
+}
+
+if(req.body.maxAge || req.body.minAge) {
+  Voter.searchAge(req.body).then(dataVoter => {
+    res.render('voter', {error: false, dataVoter: dataVoter})
+  }).catch(error => {
+    res.render('voter', {error: true, dataVoter: false})
+  })
+}
+*/
